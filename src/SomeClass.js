a3e4167073ea5fc259418eaa8e09d7f3e92e095a
Daniil Zavyalov <dezavv1@gmail.com>
2022-12-27 19:45:32 +0600

    Adding an element to an array

--- src/SomeClass.js
+++ src/SomeClass.js
@@ -3,7 +3,13 @@ import React, { Component } from "react";
 export default class SomeClass extends Component {
   constructor() {
     super();
-    this.state = { color: "red", someList: [{ title: "SomeFirstElement" }, { title: "SomeSecondElement"}, { title: "SomeThirdElement" }] };
+    this.state = {
+      color: "red",
+      value: '',
+      someList: [{ title: "SomeFirstElement" }, { title: "SomeSecondElement"}, { title: "SomeThirdElement" }]
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   renderSomeListItem({ item }) {
@@ -14,6 +20,17 @@ export default class SomeClass extends Component {
     return this.state.someList.map((item, index) => {
       return this.renderSomeListItem({ item });
     });
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      someList: this.state.someList.concat([{ title: this.state.value }])
+    });
+    event.preventDefault();
   }
 
   counter() {
@@ -28,6 +45,13 @@ export default class SomeClass extends Component {
         <div style={{ backgroundColor: this.state.color }}>Hello</div>
         {this.counter()}
         {this.renderSomeList()}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title:
+            <input type="text" value={ this.state.value } onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
